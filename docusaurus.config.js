4684bec9995736726da65fd7fc2dd999a93071c8
Yuren Ju <yurenju@gmail.com>
2022-02-28 15:56:38 +0800

    fix: use correc edit github page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/lun-dao/LunDAO/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/lun-dao/LunDAO/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/lun-dao/LunDAO/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/lun-dao/LunDAO/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
